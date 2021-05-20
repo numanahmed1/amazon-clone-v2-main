178ebc8f558494b72abe7ab24bdfe4e7a83d37d6
numanahmed1 <numanahmed317@gmail.com>
2021-05-21 04:08:07 +0600

    Amazon clone homepage

---
+++ src/components/ProductFeed.js
@@ -0,0 +1,30 @@
+import Product from "./Product";
+
+function ProductFeed({ products }) {
+  console.log(products);
+  return (
+    <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto">
+      {products.slice(0, 4).map((product) => (
+        <Product key={product.id} product={product} />
+      ))}
+
+      <img
+        className="md:col-span-full"
+        src="https://links.papareact.com/dyz"
+        alt=""
+      />
+
+      <div className="md:col-span-2">
+        {products.slice(4, 5).map((product) => (
+          <Product key={product.id} product={product} />
+        ))}
+      </div>
+
+      {products.slice(5, products.length).map((product) => (
+        <Product key={product.id} product={product} />
+      ))}
+    </div>
+  );
+}
+
+export default ProductFeed;
